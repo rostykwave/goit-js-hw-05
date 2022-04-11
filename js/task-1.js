a04eef997d884fce93c1381b93c21d753af1c37b
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-04-11 15:18:37 +0300

    Update task-1.js

---
+++ js/task-1.js
@@ -0,0 +1,32 @@
+class Account{
+    constructor({login, email}) {
+        this.login = login;
+        this.email = email;
+    }
+
+    getInfo() {
+        return `Login: ${this.login}, Email: ${this.email}`;
+    }
+
+} 
+
+console.log(Account.prototype.getInfo); // function
+
+const mango = new Account({
+  login: 'Mangozedog',
+  email: 'mango@dog.woof',
+});
+
+mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+
+const poly = new Account({
+  login: 'Poly',
+  email: 'poly@mail.com',
+});
+
+poly.getInfo(); // Login: Poly, Email: poly@mail.com
+
+
+console.log(mango.getInfo());
+console.log(poly.getInfo());
